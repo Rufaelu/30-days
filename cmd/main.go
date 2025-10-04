61c175558dec114801eb40777628a243662323d0
Rufaelu <niamraf12@gmail.com>
2025-10-04 13:07:02 +0300

    First Commit

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+
+	e := echo.New()
+	e.GET("/books", func(e echo.Context) error {
+
+		return (e.JSON(http.StatusAccepted, "Ok"))
+	})
+
+	e.Start(":8080")
+
+}
